295812cd80ac222709b013472f3cf4cfe59f7b11
mknarciso <murilokn@gmail.com>
2016-04-28 21:06:30 +0000

    Comentários git push origin master

--- LabForster_parte2.cpp
+++ LabForster_parte2.cpp
@@ -63,50 +63,18 @@ string lineOk(string a, string b){
     return result;
 }
 
-void markVisited(string a, int b){
-    //cout << "Retirar da string " << not_visited[b] << " os campos visitados: " << a << endl;
-    //cout << a.size() << endl << not_visited[b][0] << endl;
-    for(int j=0; j<a.size();){
-        for(int i=0; i<not_visited[b].size();i++){
-            int k = i;
-            //cout << not_visited[b].c_str()[i] << " = " << a[0] << "?\n" ; 
-            if(a[0]==not_visited[b].c_str()[i]){
-                //cout << "SIM!" << endl;
-                not_visited[b].erase(not_visited[b].begin() + k);
-                i--;
-            }
-        }
-        a.erase(a.begin());
-    }
-    //cout << "Resultado: " <<not_visited[b]<< endl;
-    //cout << lineOk("XXXXXXXXXXXXX",not_visited[b]);
-}
-
+//Verifica se todos os subgrupos já acharam um "pai"
 bool allVisited(){
     bool result = true;
     for (int i=0; i<not_visited.size(); i++){
         if (not_visited[i]!=""){
             result = false;
-            //cout  << "Ainda não visitado em "<<i<<":"<<not_visited[i]<<endl;
         }
     }
-
     return result;
 }
 
-
-/*bool newItem(string str){
-    for (int j=0; j<set.size();j++){
-        if (set[j].size()<str.size()){
-            if(isSubGroup(set[j],str)){
-                if(otherPath(set[j],str))
-                    return false;
-            }
-        }
-    }
-    return true;
-}*/
-
+//Verifica se entre dois subgrupos já existe um caminho
 bool hasPath(string a, string b){
     for(int i=0; i<set.size(); i++){
         if (a.size()<set[i].size()&&b.size()>set[i].size()){
@@ -118,41 +86,10 @@ bool hasPath(string a, string b){
     return false;
 }
 
-
-/*bool newItem(string str, int order){
-    //cout << str << endl;
-    bool result = true;
-    for (int i=0; i<str.size(); i++){
-        //cout<< i << "Analisando " << str[i] << endl;
-        for (int j=0; j<set.size();j++){
-            if (set[j].size()<str.size()){
-                for(int k=0;k<set[j].size();k++){
-                    //cout << set[j][k] << " = " << str[i] << "???\n";
-                    if(set[j][k]==str[i]){
-                        //std::cout << "FALSE" << std::endl;
-                        result = false;
-                    }
-                }
-                //cout << "fim da análise de " << set[j] << " bool: "<< result << endl;
-                
-            }
-        }
-        if(result){
-            //std::cout << "return true" << std::endl;
-            return true;
-        }
-        else
-            result = true;
-    }
-    return false;
-    
-}*/
-
 int main(){
-    //isSubGroup("abc","acdb");
-    //isSubGroup("1abc","abc ");
     vector<string> set_prev;
     string previous, actual;
+    //Percorre os 8 arquivos fornecidos pela primeira parte do exercício
     for(int i=1 ; i<=8 ; i++ ){
         ifstream myFile;
         ofstream myFileOutput;
@@ -160,76 +97,43 @@ int main(){
         stringstream ss;
         ss << i;
         myFile.open("subgroups table " + ss.str() + ".txt",ifstream::in);
+        //Inicia o algoritmo se existir o arquivo
         if(myFile){
             myFileOutput.open("grafo " + ss.str() + ".txt",ifstream::out);
+            // Gera o conjunto prévio de strings
             while (!myFile.eof()){
                 string line;
                 getline(myFile,line);
                 set_prev.push_back(line);
             }
+            // Ordena as strings por tamanho
             set = setSort(set_prev);
+            // Vetor auxiliar para verificação de fim do programa
             not_visited = set;
+            // Início do arquivo de saída
             myFileOutput << "digraph mygraph{\n";
             set.pop_back();
-            //cout << set.back()<<endl;
+            // Verifica para todas as strings do conjunto quais caminhos possui
             while (set.size() >1){
-                //actual = set[set.size()-1];
-                //set.pop_back();
+                // Seleciona a última da fila
                 actual = set.back();
                 set.pop_back();
-                //set.erase(set.begin());
-                //int next_order = set.back().size() - 1;
-                int next_order = 1;//set[1].size();
-                //cout << set[1].size() << endl;
-                /*for (int j=0; j<set.size(); j++){
-                    if(next_order==-1||(set[j].size()<next_order&&set[j].size()>actual.size()))
-                        next_order=set[j].size();
-                }*/
+                int next_order = 1;
+                // Percorre as outras strings do conjunto verificando caminhos possíveis, em níveis de ordem crescente
                 while(!(allVisited()||next_order>30)){
                     for (int j=0; j<set.size(); j++){
-                        //cout <<"é subgrupo:?"<< actual<< "  de  "  <<set[j]<< "   " <<isSubGroup(actual,set[j]) << set[j].size()<<next_order << endl;
-                        if (isSubGroup(actual,set[j])&&(set[j].size()==next_order)&&!hasPath(actual,set[j])){//newItem(set[j])){
-                            cout << lineOk(actual,set[j]);
+                        // Adiciona cada caminho se for um Subgrupo, for da ordem esperada nesse loop, e não possuir outro caminho entre as pontas 
+                        if (isSubGroup(actual,set[j])&&(set[j].size()==next_order)&&!hasPath(actual,set[j])){
+                            //Com as condições cumpridas adiciona o arquivo à saída
+                            //cout << lineOk(actual,set[j]);
                             myFileOutput << lineOk(actual,set[j]);
-                            //markVisited(actual,j);
-                            //cout << j << endl;
                         }
                     }
-                    //cout << next_order << endl;
-                    //next_order--;
                     next_order++;
                 }
                 
             }
-            /*previous = set[set.size()-1];
-            set.pop_back();
-            while (set.size() > 0){
-                actual = set[set.size()-1];
-                set.pop_back(); 
-                myFileOutput << "   \"{ " << previous[0] << " " ;
-                for(int i=1;i<previous.size();i++)
-                    myFileOutput << ", " << previous[i] << " " ;
-                myFileOutput << "}\" -> \"{ " << actual[0] << " " ;
-                for(int i=1;i<actual.size();i++)
-                    myFileOutput << ", " << actual[i] << " " ;
-                myFileOutput << "}\"\n";
-                previous = actual;
-            }*/
-            /*set.pop_back();
-            previous = set[set.size()-1];
-            set.pop_back();
-            while (set.size() > 0){
-                actual = set[set.size()-1];
-                set.pop_back(); 
-                myFileOutput << "   \"{ " << previous[0] << " " ;
-                for(int i=1;i<previous.size();i++)
-                    myFileOutput << ", " << previous[i] << " " ;
-                myFileOutput << "}\" -> \"{ " << actual[0] << " " ;
-                for(int i=1;i<actual.size();i++)
-                    myFileOutput << ", " << actual[i] << " " ;
-                myFileOutput << "}\"\n";
-                previous = actual;
-            }*/
+            //Fecha os arquivos
             myFileOutput << "}\n";    
             myFileOutput.close();
             set.clear();
